fe3af4c48b780ea6bf2bd6ebbe7ee7d61486f61f
lucascanni <lucas.cannizzro@estiam.com>
2023-03-14 17:39:40 +0100

    fin cours 14/03/2023

--- routes/transactions.js
+++ routes/transactions.js
@@ -3,9 +3,22 @@ var express = require('express');
 const { time } = require('console');
 var router = express.Router();
 
-/* GET users listing. */
+const TransactionsSchema = new mongoose.Schema(
+  {
+    id: String,
+    type: String,
+    categorie: String,
+    montant: Number,
+    date: Date
+  },
+  { strict: false }
+);
+const Transactions = mongoose.model('transactions', TransactionsSchema);
+
+
+/* GET transaction listing. */
 router.get('/', async function(req, res, next) {
-  const TransactionsSchema = new mongoose.Schema(
+  /*const TransactionsSchema = new mongoose.Schema(
     {
       id: String,
       type: String,
@@ -16,10 +29,41 @@ router.get('/', async function(req, res, next) {
     { strict: false }
   );
   const Transactions = mongoose.model('transactions', TransactionsSchema);
-
+*/
   const transactions = await Transactions.find({});
 
   res.send(transactions);
 });
+
+router.get('/:id', (req, res) => {
+  const transaction = transactions.find(u => u.id === parseInt(req.params.id));
+  if (!transactions) {
+    return res.status(404).send('User not found');
+  }
+  res.json(transaction);
+});
+
+router.post('/', (req, res) => {
+  /*const transactions = {
+    type: req.body.type,
+    categorie: req.body.categorie,
+    montant: req.body.montant,
+    date: req.body.date
+  };
+  transactions.push(transactions);
+  res.json(transactions);*/
+
+  const newTransaction = new Transactions(req.body);
+  newTransaction.save((err, transaction) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(transaction);
+    }
+  });
+
+}); 
+
+
 
 module.exports = router;
