0f06ccc61242a519829684e754ffe54c52a4af04
TopNiz <nizar.ayed@upgrade-code.org>
2023-03-14 15:19:25 +0100

    Init code for mongodb connection

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
+
+dotenv.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,12 +28,12 @@ app.use('/users', usersRouter);
 app.use('/create-db', createDbRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ db/conn.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/finperso';
+
+main()
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
